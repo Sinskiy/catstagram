e524cf787bb893e7357a04666a96bfd1024914c3
Sinskiy <sinskiy@proton.me>
2023-10-06 17:35:42 +0300

    feat: add test login and fix searches stats

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,23 +1,30 @@
 <script>
   export let data
-  $: console.log(data)
+
+  let { supabase } = data
+
+  async function signInWithGoogle() {
+    await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        redirectTo: 'http://localhost:5173/profile'
+      }
+    })
+  }
 </script>
 
-<h2 class="flex flex-col items-center section-title mt-auto">
-  You're not logged in
-</h2>
-<div class="flex flex-col gap-2 mt-12 xmd">
-  <button
-    class="button bg-secondaryContainer text-onSecondaryContainer min-w-full"
-  >
-    Log in
-  </button>
-  <button
-    class="button bg-tertiaryContainer text-onTertiaryContainer min-w-full"
-  >
-    Sign up
-  </button>
-</div>
-{#each data.data as item}
-  <h1>{item.name}</h1>
-{/each}
+{#if data.session}
+  <h1>logged in!</h1>
+{:else}
+  <h2 class="flex flex-col items-center section-title mt-auto">
+    You're not logged in
+  </h2>
+  <div class="flex flex-col gap-2 mt-12 xmd">
+    <button
+      on:click={signInWithGoogle}
+      class="button bg-tertiaryContainer text-onTertiaryContainer min-w-full"
+    >
+      Log in with Google
+    </button>
+  </div>
+{/if}
