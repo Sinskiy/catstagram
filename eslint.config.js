a8b9db52dc375e42a4a1fd58c8b55bdf8435d177
sinskiy <sinskiy@proton.me>
2024-09-17 16:51:32 +0300

    feat: configure eslint to work with client/

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,34 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
+import js from "@eslint/js";
 import tseslint from "typescript-eslint";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 
-export default [
-  { files: ["**/*.ts"] },
-  { languageOptions: { globals: globals.node } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-];
+/** @type { import("eslint").Linter.Config[] } */
+export default tseslint.config(
+  { ignores: ["**/dist"] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["server/**/*.ts"],
+    languageOptions: { globals: globals.node },
+  },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["client/**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+    },
+  },
+);
