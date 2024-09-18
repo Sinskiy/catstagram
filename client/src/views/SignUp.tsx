1a2a8b83936d5a334ea4113072427d156a7ae3e4
sinskiy <sinskiy@proton.me>
2024-09-18 07:49:38 +0300

    feat(client): move to new @sinskiy/ui version

--- client/src/views/SignUp.tsx
+++ client/src/views/SignUp.tsx
@@ -1,6 +1,3 @@
-// UI: types
-// UI: input font-size
-// @ts-expect-error lib
 import { Button, Form, InputField } from "@sinskiy/ui";
 import Hr from "../components/Hr";
 import styled from "styled-components";
@@ -16,33 +13,22 @@ const Oauth = styled.div`
   gap: 1rem;
 `;
 
-// TODO: move to styled-components
 export default function SignUp() {
   return (
     <Section>
       <h1 className="main-heading">sign up</h1>
-      {/* UI: don't display errors if empty */}
       <Form>
-        {/* UI: remove width: 100% */}
-        {/* UI: investigate placeholder color */}
         <InputField
           label="email"
           type="email"
           placeholder="john doe"
-          style={{ width: "auto" }}
           required
         />
-        <InputField
-          label="password"
-          type="password"
-          style={{ width: "auto" }}
-          required
-        />
+        <InputField label="password" type="password" required />
         <InputField
           label="confirm password"
           id="confirm-password"
           type="password"
-          style={{ width: "auto" }}
           required
         />
       </Form>
