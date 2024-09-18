d42c1de9506f72ea992cf541c73b07821f77d9c9
sinskiy <sinskiy@proton.me>
2024-09-18 16:50:17 +0300

    feat(client): use icons for oauth buttons

--- client/src/views/Oauth.tsx
+++ client/src/views/Oauth.tsx
@@ -1,6 +1,8 @@
 import { Button } from "@sinskiy/ui";
 import styled from "styled-components";
 import Hr from "../components/Hr";
+import GithubIcon from "../icons/GithubIcon";
+import GoogleIcon from "../icons/GoogleIcon";
 
 const Styled = styled.div`
   display: flex;
@@ -9,12 +11,21 @@ const Styled = styled.div`
 `;
 
 export default function Oauth() {
+  const imgProps = {
+    alt: "",
+    width: 24,
+    height: 24,
+  };
   return (
     <>
       <Hr>or</Hr>
       <Styled>
-        <Button themeColor="primary-container">Continue with Google</Button>
-        <Button themeColor="primary-container">Continue with GitHub</Button>
+        <Button themeColor="primary-container" aria-label="sign in with google">
+          <GithubIcon {...imgProps} />
+        </Button>
+        <Button themeColor="primary-container" aria-label="sign in with google">
+          <GoogleIcon {...imgProps} />
+        </Button>
       </Styled>
     </>
   );
