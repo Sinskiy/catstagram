08c6f512fa27bb8ccb9883c5bf4081212d271f62
sinskiy <sinskiy@proton.me>
2024-09-18 19:48:26 +0300

    feat(server): use passport

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,11 +2,14 @@ import express, { json, urlencoded } from "express";
 import cors from "cors";
 import apiRouter from "./routes/index.js";
 import errorHandler from "./middlewares/errorHandler.js";
+import passport from "./configs/auth.js";
 
 const app = express();
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(json());
 app.use(urlencoded({ extended: false }));
+
+app.use(passport.session());
 
 app.use("/", apiRouter);
 
